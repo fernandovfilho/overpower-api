97b5db12c88bb927c5c746bf585bb5b92ef011a6
Fernando Vargas <fernando.vargas@asksuite.com>
2022-05-10 11:18:59 -0300

    Husky test and changed server default port

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import cors from "cors"
-import express from 'express';
+import cors from 'cors';
+import express from "express"
 import { routes } from './routes'
 
 const app = express();
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use('api/v1', routes);
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server ready at port: ${PORT}`);
